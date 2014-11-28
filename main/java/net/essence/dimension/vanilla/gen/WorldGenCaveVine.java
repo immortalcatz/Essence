01ba93ced375981655ac9b523a676fd4e211ea3a
TheSlayerMC <brock.kerley@hotmail.com>
2014-11-28 23:07:11 +1030

    Non crashable

--- main/java/net/essence/dimension/vanilla/gen/WorldGenCaveVine.java
+++ main/java/net/essence/dimension/vanilla/gen/WorldGenCaveVine.java
@@ -26,8 +26,8 @@ public class WorldGenCaveVine extends WorldGenerator {
 					for (int j = 0; j < i; ++j) {
 						EnumFacing enumfacing = aenumfacing[j];
 						if(EssenceBlocks.caveVine.canPlaceBlockOnSide(w, pos, enumfacing)) {
-							IBlockState iblockstate = EssenceBlocks.caveVine.getDefaultState().withProperty(BlockVine.field_176273_b, Boolean.valueOf(enumfacing == EnumFacing.NORTH)).withProperty(BlockVine.field_176278_M, Boolean.valueOf(enumfacing == EnumFacing.EAST)).withProperty(BlockVine.field_176279_N, Boolean.valueOf(enumfacing == EnumFacing.SOUTH)).withProperty(BlockVine.field_176280_O, Boolean.valueOf(enumfacing == EnumFacing.WEST));
-							w.setBlockState(pos, iblockstate, 2);
+							//IBlockState iblockstate = EssenceBlocks.caveVine.getDefaultState().withProperty(BlockVine.field_176273_b, Boolean.valueOf(enumfacing == EnumFacing.NORTH)).withProperty(BlockVine.field_176278_M, Boolean.valueOf(enumfacing == EnumFacing.EAST)).withProperty(BlockVine.field_176279_N, Boolean.valueOf(enumfacing == EnumFacing.SOUTH)).withProperty(BlockVine.field_176280_O, Boolean.valueOf(enumfacing == EnumFacing.WEST));
+							//w.setBlockState(pos, iblockstate, 2);
 							break;
 						}
 					}
