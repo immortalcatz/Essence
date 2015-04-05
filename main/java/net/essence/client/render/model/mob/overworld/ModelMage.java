2a6433393ab8c2b7e4fc0da490c964581b531b33
Eternaldoom <mezack@icloud.com>
2015-04-04 22:33:12 -0700

    New mage models

--- main/java/net/essence/client/render/model/mob/overworld/ModelMage.java
+++ main/java/net/essence/client/render/model/mob/overworld/ModelMage.java
@@ -1,14 +1,11 @@
-package net.essence.client.render.model.mob.frozen;
-
-import org.lwjgl.opengl.GL11;
+package net.essence.client.render.model.mob.overworld;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.MathHelper;
 
-public class ModelFrozenMerchant extends ModelBase {
+public class ModelMage extends ModelBase {
 	ModelRenderer head;
 	ModelRenderer body;
 	ModelRenderer rightarm;
@@ -20,43 +17,43 @@ public class ModelFrozenMerchant extends ModelBase {
 	ModelRenderer hatTop;
 	ModelRenderer puff;
 
-	public ModelFrozenMerchant() {
+	public ModelMage() {
 		textureWidth = 128;
 		textureHeight = 64;
 
 		head = new ModelRenderer(this, 0, 0);
 		head.addBox(-4F, -8F, -4F, 8, 8, 8);
-		head.setRotationPoint(0F, 6F, 0F);
+		head.setRotationPoint(0F, 0F, 0F);
 		head.setTextureSize(128, 64);
 		head.mirror = true;
 		setRotation(head, 0F, 0F, 0F);
 		body = new ModelRenderer(this, 16, 16);
-		body.addBox(-4F, 0F, -2F, 8, 10, 4);
-		body.setRotationPoint(0F, 6F, 0F);
+		body.addBox(-4F, 0F, -2F, 8, 12, 4);
+		body.setRotationPoint(0F, 0F, 0F);
 		body.setTextureSize(128, 64);
 		body.mirror = true;
 		setRotation(body, 0F, 0F, 0F);
 		rightarm = new ModelRenderer(this, 40, 16);
-		rightarm.addBox(-3F, -2F, -2F, 4, 10, 4);
-		rightarm.setRotationPoint(-5F, 8F, 0F);
+		rightarm.addBox(-3F, -2F, -2F, 4, 12, 4);
+		rightarm.setRotationPoint(-5F, 2F, 0F);
 		rightarm.setTextureSize(128, 64);
 		rightarm.mirror = true;
 		setRotation(rightarm, 0F, 0F, 0F);
-		leftarm = new ModelRenderer(this, 40, 16);
-		leftarm.addBox(-1F, -2F, -2F, 4, 10, 4);
-		leftarm.setRotationPoint(5F, 8F, 0F);
+		leftarm = new ModelRenderer(this, 40, 32);
+		leftarm.addBox(-1F, -2F, -2F, 4, 12, 4);
+		leftarm.setRotationPoint(5F, 2F, 0F);
 		leftarm.setTextureSize(128, 64);
 		leftarm.mirror = true;
 		setRotation(leftarm, 0F, 0F, 0F);
 		rightleg = new ModelRenderer(this, 0, 16);
-		rightleg.addBox(-2F, 0F, -2F, 4, 8, 4);
-		rightleg.setRotationPoint(-2F, 16F, 0F);
+		rightleg.addBox(-2F, 0F, -2F, 4, 12, 4);
+		rightleg.setRotationPoint(-2F, 12F, 0F);
 		rightleg.setTextureSize(128, 64);
 		rightleg.mirror = true;
 		setRotation(rightleg, 0F, 0F, 0F);
 		leftleg = new ModelRenderer(this, 0, 16);
-		leftleg.addBox(-2F, 0F, -2F, 4, 8, 4);
-		leftleg.setRotationPoint(2F, 16F, 0F);
+		leftleg.addBox(-2F, 0F, -2F, 4, 12, 4);
+		leftleg.setRotationPoint(2F, 12F, 0F);
 		leftleg.setTextureSize(128, 64);
 		leftleg.mirror = true;
 		setRotation(leftleg, 0F, 0F, 0F);
@@ -94,18 +91,12 @@ public class ModelFrozenMerchant extends ModelBase {
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
 		super.render(entity, f, f1, f2, f3, f4, f5);
 		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-		GlStateManager.pushMatrix();
-	    GlStateManager.enableBlend();
-	    GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-	    GlStateManager.depthMask(true);
-	    GL11.glEnable(GL11.GL_ALPHA_TEST);
 		head.render(f5);
 		body.render(f5);
 		rightarm.render(f5);
 		leftarm.render(f5);
 		rightleg.render(f5);
 		leftleg.render(f5);
-		GlStateManager.popMatrix();
 	}
 
 	private void setRotation(ModelRenderer model, float x, float y, float z) {
@@ -121,6 +112,8 @@ public class ModelFrozenMerchant extends ModelBase {
 		this.head.rotateAngleX= f4 / 57.29578f;
 		this.rightleg.rotateAngleX = MathHelper.cos(f * 0.6662F + (float)Math.PI) * f1;
 		this.leftleg.rotateAngleX = MathHelper.cos(f * 0.6662F) * f1;
+		this.leftarm.rotateAngleX = MathHelper.cos(f * 0.6662F + (float)Math.PI) * f1;
+		this.rightarm.rotateAngleX = MathHelper.cos(f * 0.6662F) * f1;
 	}
 
 }
--- main/java/net/essence/client/render/model/mob/overworld/ModelMageTransparent.java
+++ main/java/net/essence/client/render/model/mob/overworld/ModelMageTransparent.java
@@ -1,4 +1,4 @@
-package net.essence.client.render.model.mob.frozen;
+package net.essence.client.render.model.mob.overworld;
 
 import org.lwjgl.opengl.GL11;
 
@@ -8,7 +8,7 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.MathHelper;
 
-public class ModelFrozenMerchant extends ModelBase {
+public class ModelMageTransparent extends ModelBase {
 	ModelRenderer head;
 	ModelRenderer body;
 	ModelRenderer rightarm;
@@ -20,43 +20,43 @@ public class ModelFrozenMerchant extends ModelBase {
 	ModelRenderer hatTop;
 	ModelRenderer puff;
 
-	public ModelFrozenMerchant() {
+	public ModelMageTransparent() {
 		textureWidth = 128;
 		textureHeight = 64;
 
 		head = new ModelRenderer(this, 0, 0);
 		head.addBox(-4F, -8F, -4F, 8, 8, 8);
-		head.setRotationPoint(0F, 6F, 0F);
+		head.setRotationPoint(0F, 0F, 0F);
 		head.setTextureSize(128, 64);
 		head.mirror = true;
 		setRotation(head, 0F, 0F, 0F);
 		body = new ModelRenderer(this, 16, 16);
-		body.addBox(-4F, 0F, -2F, 8, 10, 4);
-		body.setRotationPoint(0F, 6F, 0F);
+		body.addBox(-4F, 0F, -2F, 8, 12, 4);
+		body.setRotationPoint(0F, 0F, 0F);
 		body.setTextureSize(128, 64);
 		body.mirror = true;
 		setRotation(body, 0F, 0F, 0F);
 		rightarm = new ModelRenderer(this, 40, 16);
-		rightarm.addBox(-3F, -2F, -2F, 4, 10, 4);
-		rightarm.setRotationPoint(-5F, 8F, 0F);
+		rightarm.addBox(-3F, -2F, -2F, 4, 12, 4);
+		rightarm.setRotationPoint(-5F, 2F, 0F);
 		rightarm.setTextureSize(128, 64);
 		rightarm.mirror = true;
 		setRotation(rightarm, 0F, 0F, 0F);
-		leftarm = new ModelRenderer(this, 40, 16);
-		leftarm.addBox(-1F, -2F, -2F, 4, 10, 4);
-		leftarm.setRotationPoint(5F, 8F, 0F);
+		leftarm = new ModelRenderer(this, 40, 32);
+		leftarm.addBox(-1F, -2F, -2F, 4, 12, 4);
+		leftarm.setRotationPoint(5F, 2F, 0F);
 		leftarm.setTextureSize(128, 64);
 		leftarm.mirror = true;
 		setRotation(leftarm, 0F, 0F, 0F);
 		rightleg = new ModelRenderer(this, 0, 16);
-		rightleg.addBox(-2F, 0F, -2F, 4, 8, 4);
-		rightleg.setRotationPoint(-2F, 16F, 0F);
+		rightleg.addBox(-2F, 0F, -2F, 4, 12, 4);
+		rightleg.setRotationPoint(-2F, 12F, 0F);
 		rightleg.setTextureSize(128, 64);
 		rightleg.mirror = true;
 		setRotation(rightleg, 0F, 0F, 0F);
 		leftleg = new ModelRenderer(this, 0, 16);
-		leftleg.addBox(-2F, 0F, -2F, 4, 8, 4);
-		leftleg.setRotationPoint(2F, 16F, 0F);
+		leftleg.addBox(-2F, 0F, -2F, 4, 12, 4);
+		leftleg.setRotationPoint(2F, 12F, 0F);
 		leftleg.setTextureSize(128, 64);
 		leftleg.mirror = true;
 		setRotation(leftleg, 0F, 0F, 0F);
@@ -121,6 +121,8 @@ public class ModelFrozenMerchant extends ModelBase {
 		this.head.rotateAngleX= f4 / 57.29578f;
 		this.rightleg.rotateAngleX = MathHelper.cos(f * 0.6662F + (float)Math.PI) * f1;
 		this.leftleg.rotateAngleX = MathHelper.cos(f * 0.6662F) * f1;
+		this.leftarm.rotateAngleX = MathHelper.cos(f * 0.6662F + (float)Math.PI) * f1;
+		this.rightarm.rotateAngleX = MathHelper.cos(f * 0.6662F) * f1;
 	}
 
 }
