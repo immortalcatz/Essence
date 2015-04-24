7029c9ba3fe0eb7910dc0f056cf1b26fc6b45e14
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2015-04-24 13:29:48 -0700

    fixed texture

--- main/java/net/essence/util/Textures.java
+++ main/java/net/essence/util/Textures.java
@@ -82,7 +82,6 @@ public class Textures {
     public static final ResourceLocation woodCreature = addMob("woodCreature");
     public static final ResourceLocation roc = addMob("roc");
 
-
     private static ResourceLocation addMob(String tex){
     	return new ResourceLocation(prefix + tex + ".png");
     }
