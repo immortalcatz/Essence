8f13860c1c8d19595f9e609c0b1acee848a1712b
TheSlayerMC <brock.kerley@hotmail.com>
2015-05-07 18:54:11 +0930

    Meh

--- main/java/net/essence/client/render/model/mob/boil/ModelBoilTrader.java
+++ main/java/net/essence/client/render/model/mob/boil/ModelBoilTrader.java
@@ -7,46 +7,45 @@ import net.minecraft.util.MathHelper;
 
 public class ModelBoilTrader extends ModelBase {
 
-    ModelRenderer head;
-    ModelRenderer rightarm;
-    ModelRenderer leftarm;
-    ModelRenderer top;
+	ModelRenderer head;
+	ModelRenderer rightarm;
+	ModelRenderer leftarm;
+	ModelRenderer top;
 
 	public ModelBoilTrader() {
 		textureWidth = 64;
-	    textureHeight = 64;
-	    
-	      head = new ModelRenderer(this, 0, 0);
-	      head.addBox(-4F, -8F, -4F, 8, 8, 8);
-	      head.setRotationPoint(0F, 0F, 0F);
-	      head.setTextureSize(64, 64);
-	      setRotation(head, 0F, 0F, 0F);
-	      rightarm = new ModelRenderer(this, 40, 16);
-	      rightarm.addBox(-3F, -2F, -2F, 2, 12, 4);
-	      rightarm.setRotationPoint(-3F, -8F, 0F);
-	      rightarm.setTextureSize(64, 64);
-	      setRotation(rightarm, 0F, 0F, 0F);
-	      leftarm = new ModelRenderer(this, 40, 16);
-	      leftarm.addBox(-1F, -2F, -2F, 2, 12, 4);
-	      leftarm.setRotationPoint(5F, -8F, 0F);
-	      leftarm.setTextureSize(64, 64);
-	      setRotation(leftarm, 0F, 0F, 0F);
-	      top = new ModelRenderer(this, 0, 16);
-	      top.addBox(0F, 0F, 0F, 10, 1, 10);
-	      top.setRotationPoint(-5F, -9F, -5F);
-	      top.setTextureSize(64, 64);
-	      setRotation(top, 0F, 0F, 0F);
+		textureHeight = 64;
+
+		head = new ModelRenderer(this, 0, 0);
+		head.addBox(-4F, -8F, -4F, 8, 8, 8);
+		head.setRotationPoint(0F, 0F, 0F);
+		head.setTextureSize(64, 64);
+		setRotation(head, 0F, 0F, 0F);
+		rightarm = new ModelRenderer(this, 40, 16);
+		rightarm.addBox(-3F, -2F, -2F, 2, 12, 4);
+		rightarm.setRotationPoint(-3F, -8F, 0F);
+		rightarm.setTextureSize(64, 64);
+		setRotation(rightarm, 0F, 0F, 0F);
+		leftarm = new ModelRenderer(this, 40, 16);
+		leftarm.addBox(-1F, -2F, -2F, 2, 12, 4);
+		leftarm.setRotationPoint(5F, -8F, 0F);
+		leftarm.setTextureSize(64, 64);
+		setRotation(leftarm, 0F, 0F, 0F);
+		top = new ModelRenderer(this, 0, 16);
+		top.addBox(0F, 0F, 0F, 10, 1, 10);
+		top.setRotationPoint(-5F, -9F, -5F);
+		top.setTextureSize(64, 64);
+		setRotation(top, 0F, 0F, 0F);
 	}
 
 	@Override
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
 		super.render(entity, f, f1, f2, f3, f4, f5);
 		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-	    head.render(f5);
-	    rightarm.render(f5);
-	    leftarm.render(f5);
-	    top.render(f5);
-
+		head.render(f5);
+		rightarm.render(f5);
+		leftarm.render(f5);
+		top.render(f5);
 	}
 
 	private void setRotation(ModelRenderer model, float x, float y, float z) {
