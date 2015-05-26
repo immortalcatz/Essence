34c28b8a2d457bbd917f490e507c8bb53204cada
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2015-05-25 23:30:03 -0700

    More textures

--- main/java/net/essence/EssenceBlocks.java
+++ main/java/net/essence/EssenceBlocks.java
@@ -122,6 +122,8 @@ public class EssenceBlocks {
 	public static final Block depthsPlank = new BlockMod(EnumMaterialTypes.WOOD, "depthsPlank", 1.0F);
 	public static final Block depthsStairs = new BlockModStairs(depthsPlank, "depthsStairs");
 	public static final Block depthsLeaves = new BlockModLeaves("depthsLeaves", 1.0F);
+	public static final Block darkBrick = new BlockMod("darkBrick", 2.0F);
+	public static final Block darkFloor = new BlockMod("darkFloor", 2.0F);
 
 	//public static final BlockMod godDirt = new BlockMod(EnumMaterialTypes.DIRT, "godDirt", 2.0F);
 	//public static final Block godGrass = new BlockModGrass(godDirt, "godGrass", 2.0F);
