584751af56a359e07f11aa654d90460e73a70b4e
TheSlayerMC <brock.kerley@hotmail.com>
2015-06-05 13:35:28 +0930

    made mob heath render in f3 mode

--- main/java/net/essence/client/render/RenderModMob.java
+++ main/java/net/essence/client/render/RenderModMob.java
@@ -31,9 +31,11 @@ public class RenderModMob extends RenderLiving {
 
 	@Override
 	public void doRender(Entity entity, double x, double y, double z, float f, float partialTicks) {
-		if(Config.showEntityHealth) {
-			EntityLivingBase e = (EntityLivingBase)entity;
-			renderHealth(e, EnumChatFormatting.GREEN + "Health: " + EnumChatFormatting.AQUA + (int)e.getHealth() + "/" + (int)e.getMaxHealth(), x, y, z, Config.entityHealthDistance);
+		if(Minecraft.getMinecraft().gameSettings.showDebugInfo) {
+			if(Config.showEntityHealth) {
+				EntityLivingBase e = (EntityLivingBase)entity;
+				renderHealth(e, EnumChatFormatting.GREEN + "Health: " + EnumChatFormatting.AQUA + (int)e.getHealth() + "/" + (int)e.getMaxHealth(), x, y, z, Config.entityHealthDistance);
+			}
 		}
 		super.doRender(entity, x, y, z, f, partialTicks);
 	}
