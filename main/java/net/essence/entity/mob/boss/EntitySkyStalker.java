30b433b556e3b978cb6f633b0efa398549de5cb0
Eternaldoom <eternaldoom123@gmail.com>
2015-11-10 21:25:18 -0800

    flight

--- main/java/net/essence/entity/mob/boss/EntitySkyStalker.java
+++ main/java/net/essence/entity/mob/boss/EntitySkyStalker.java
@@ -5,8 +5,10 @@ import java.util.Random;
 import net.essence.EssenceItems;
 import net.essence.entity.MobStats;
 import net.essence.entity.mob.cloudia.EntitySkyEel;
+import net.essence.entity.mob.euca.EntityShimmerer;
 import net.essence.entity.projectile.EntityShimmererProjectile;
 import net.essence.enums.EnumSounds;
+import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIFindEntityNearestPlayer;
@@ -21,12 +23,12 @@ import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 import net.slayer.api.entity.EntityEssenceBoss;
 
-public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker(World par1World) {
+public class EntitySkyStalker extends EntityEssenceBoss {
+	public EntitySkyStalker(World par1World) {
 		super(par1World);
 		this.moveHelper = new EntitySkyStalker.MoveHelper();
 		this.tasks.addTask(5, new EntitySkyStalker.AIRandomFly());
-		this.tasks.addTask(7, new EntitySkyStalker.AIFireballAttack());
-        this.tasks.addTask(7, new EntitySkyStalker.AILookAround());
+		this.tasks.addTask(7, new EntitySkyStalker.AILookAround());
 		this.targetTasks.addTask(1, new EntityAIFindEntityNearestPlayer(this));
 		setSize(0.7F, 1.2F);
 	}
@@ -50,35 +52,42 @@ public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker
 	public EnumSounds setDeathSound() {
 		return EnumSounds.SHIMMERER_DEATH;
 	}
-	
+
 	@Override
 	protected float getSoundVolume() {
-        return 10.0F;
-    }
-	
+		return 10.0F;
+	}
+
 	@Override
 	public boolean getCanSpawnHere() {
-        return this.rand.nextInt(15) == 0 && super.getCanSpawnHere() && this.worldObj.getDifficulty() != EnumDifficulty.PEACEFUL;
-    }
+		return this.rand.nextInt(15) == 0 && super.getCanSpawnHere()
+				&& this.worldObj.getDifficulty() != EnumDifficulty.PEACEFUL;
+	}
 
-    @Override
-    public int getMaxSpawnedInChunk() {
-        return 3;
-    }
+	@Override
+	public int getMaxSpawnedInChunk() {
+		return 3;
+	}
 
 	@Override
 	protected void dropFewItems(boolean b, int j) {
-		switch(rand.nextInt(3)) {
-		case 0: dropItem(EssenceItems.skyPiercer, 128); break;
-		case 1: dropItem(EssenceItems.fluffyBlade, 1); break;
-		case 2: dropItem(EssenceItems.fluffyBow, 1); break;
+		switch (rand.nextInt(3)) {
+		case 0:
+			dropItem(EssenceItems.skyPiercer, 128);
+			break;
+		case 1:
+			dropItem(EssenceItems.fluffyBlade, 1);
+			break;
+		case 2:
+			dropItem(EssenceItems.fluffyBow, 1);
+			break;
 		}
 	}
 
 	@Override
 	protected void entityInit() {
 		super.entityInit();
-		this.dataWatcher.addObject(16, Byte.valueOf((byte)0));
+		this.dataWatcher.addObject(16, Byte.valueOf((byte) 0));
 	}
 
 	@Override
@@ -87,7 +96,62 @@ public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker
 	}
 
 	public void setFire(boolean b) {
-		this.dataWatcher.updateObject(16, Byte.valueOf((byte)(b ? 1 : 0)));
+		this.dataWatcher.updateObject(16, Byte.valueOf((byte) (b ? 1 : 0)));
+	}
+
+	@Override
+	public double setAttackDamage(MobStats s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void fall(float distance, float damageMultiplier) {
+	}
+
+	@Override
+	protected void func_180433_a(double p_180433_1_, boolean p_180433_3_, Block p_180433_4_, BlockPos p_180433_5_) {
+	}
+
+	@Override
+	public void moveEntityWithHeading(float p_70612_1_, float p_70612_2_) {
+		if (this.isInWater()) {
+			this.moveFlying(p_70612_1_, p_70612_2_, 0.02F);
+			this.moveEntity(this.motionX, this.motionY, this.motionZ);
+			this.motionX *= 0.800000011920929D;
+			this.motionY *= 0.800000011920929D;
+			this.motionZ *= 0.800000011920929D;
+		} else if (this.isInLava()) {
+			this.moveFlying(p_70612_1_, p_70612_2_, 0.02F);
+			this.moveEntity(this.motionX, this.motionY, this.motionZ);
+			this.motionX *= 0.5D;
+			this.motionY *= 0.5D;
+			this.motionZ *= 0.5D;
+		} else {
+			float f2 = 0.91F;
+
+			if (this.onGround) {
+				f2 = this.worldObj.getBlockState(new BlockPos(MathHelper.floor_double(this.posX),
+						MathHelper.floor_double(this.getEntityBoundingBox().minY) - 1,
+						MathHelper.floor_double(this.posZ))).getBlock().slipperiness * 0.91F;
+			}
+
+			float f3 = 0.16277136F / (f2 * f2 * f2);
+			this.moveFlying(p_70612_1_, p_70612_2_, this.onGround ? 0.1F * f3 : 0.02F);
+			f2 = 0.91F;
+
+			if (this.onGround) {
+				f2 = this.worldObj.getBlockState(new BlockPos(MathHelper.floor_double(this.posX),
+						MathHelper.floor_double(this.getEntityBoundingBox().minY) - 1,
+						MathHelper.floor_double(this.posZ))).getBlock().slipperiness * 0.91F;
+			}
+
+			this.moveEntity(this.motionX, this.motionY, this.motionZ);
+			this.motionX *= (double) f2;
+			this.motionY *= (double) f2;
+			this.motionZ *= (double) f2;
+		}
+
 	}
 
 	private class AIRandomFly extends EntityAIBase {
@@ -100,7 +164,7 @@ public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker
 		@Override
 		public boolean shouldExecute() {
 			EntityMoveHelper entitymovehelper = this.e.getMoveHelper();
-			if(!entitymovehelper.isUpdating()) {
+			if (!entitymovehelper.isUpdating()) {
 				return true;
 			} else {
 				double d0 = entitymovehelper.func_179917_d() - this.e.posX;
@@ -119,9 +183,9 @@ public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker
 		@Override
 		public void startExecuting() {
 			Random random = this.e.getRNG();
-			double d0 = this.e.posX + (double)((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
-			double d1 = this.e.posY + (double)((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
-			double d2 = this.e.posZ + (double)((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+			double d0 = this.e.posX + (double) ((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+			double d1 = this.e.posY + (double) ((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+			double d2 = this.e.posZ + (double) ((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
 			this.e.getMoveHelper().setMoveTo(d0, d1, d2, 1.0D);
 		}
 	}
@@ -136,15 +200,15 @@ public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker
 
 		@Override
 		public void onUpdateMoveHelper() {
-			if(this.update) {
+			if (this.update) {
 				double d0 = this.posX - this.e.posX;
 				double d1 = this.posY - this.e.posY;
 				double d2 = this.posZ - this.e.posZ;
 				double d3 = d0 * d0 + d1 * d1 + d2 * d2;
-				if(this.height-- <= 0) {
+				if (this.height-- <= 0) {
 					this.height += this.e.getRNG().nextInt(5) + 2;
-					d3 = (double)MathHelper.sqrt_double(d3);
-					if(this.canMove(this.posX, this.posY, this.posZ, d3)) {
+					d3 = (double) MathHelper.sqrt_double(d3);
+					if (this.canMove(this.posX, this.posY, this.posZ, d3)) {
 						this.e.motionX += d0 / d3 * 0.1D;
 						this.e.motionY += d1 / d3 * 0.1D;
 						this.e.motionZ += d2 / d3 * 0.1D;
@@ -155,14 +219,14 @@ public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker
 			}
 		}
 
-		private boolean canMove(double x, double y, double z, double h)  {
+		private boolean canMove(double x, double y, double z, double h) {
 			double d4 = (x - this.e.posX) / h;
 			double d5 = (y - this.e.posY) / h;
 			double d6 = (z - this.e.posZ) / h;
 			AxisAlignedBB axisalignedbb = this.e.getEntityBoundingBox();
-			for(int i = 1; (double)i < h; ++i) {
+			for (int i = 1; (double) i < h; ++i) {
 				axisalignedbb = axisalignedbb.offset(d4, d5, d6);
-				if(!this.e.worldObj.getCollidingBoundingBoxes(this.e, axisalignedbb).isEmpty()) {
+				if (!this.e.worldObj.getCollidingBoundingBoxes(this.e, axisalignedbb).isEmpty()) {
 					return false;
 				}
 			}
@@ -185,7 +249,8 @@ public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker
 		@Override
 		public void updateTask() {
 			if (this.e.getAttackTarget() == null) {
-				this.e.renderYawOffset = this.e.rotationYaw = -((float)Math.atan2(this.e.motionX, this.e.motionZ)) * 180.0F / (float)Math.PI;
+				this.e.renderYawOffset = this.e.rotationYaw = -((float) Math.atan2(this.e.motionX, this.e.motionZ))
+						* 180.0F / (float) Math.PI;
 			} else {
 				EntityLivingBase entitylivingbase = this.e.getAttackTarget();
 				double d0 = 64.0D;
@@ -193,63 +258,10 @@ public class EntitySkyStalker extends EntityEssenceBoss {public EntitySkyStalker
 				if (entitylivingbase.getDistanceSqToEntity(this.e) < d0 * d0) {
 					double d1 = entitylivingbase.posX - this.e.posX;
 					double d2 = entitylivingbase.posZ - this.e.posZ;
-					this.e.renderYawOffset = this.e.rotationYaw = -((float)Math.atan2(d1, d2)) * 180.0F / (float)Math.PI;
+					this.e.renderYawOffset = this.e.rotationYaw = -((float) Math.atan2(d1, d2)) * 180.0F
+							/ (float) Math.PI;
 				}
 			}
 		}
-	}
-
-	public class AIFireballAttack extends EntityAIBase {
-		private EntitySkyStalker entity = EntitySkyStalker.this;
-		public int counter;
-
-		@Override
-		public boolean shouldExecute() {
-			return this.entity.getAttackTarget() != null;
-		}
-
-		@Override
-		public void startExecuting() {
-			this.counter = 0;
-		}
-
-		@Override
-		public void resetTask() {
-			this.entity.setFire(false);
-		}
-
-		@Override
-		public void updateTask() {
-			EntityLivingBase entitylivingbase = this.entity.getAttackTarget();
-			double d0 = 64.0D;
-
-			if(entitylivingbase.getDistanceSqToEntity(this.entity) < d0 * d0 && this.entity.canEntityBeSeen(entitylivingbase)) {
-				World world = this.entity.worldObj;
-				counter++;
-
-				if(this.counter == 20) {
-					double d1 = 4.0D;
-					Vec3 vec3 = this.entity.getLook(1.0F);
-					double d2 = entitylivingbase.posX - (this.entity.posX + vec3.xCoord * d1);
-					double d3 = entitylivingbase.getEntityBoundingBox().minY + (double)(entitylivingbase.height / 2.0F) - (0.5D + this.entity.posY + (double)(this.entity.height / 2.0F));
-					double d4 = entitylivingbase.posZ - (this.entity.posZ + vec3.zCoord * d1);
-					world.playAuxSFXAtEntity((EntityPlayer)null, 1008, new BlockPos(this.entity), 0);
-					EntityShimmererProjectile projectile = new EntityShimmererProjectile(world, this.entity, d2, d3, d4);
-					projectile.posX = this.entity.posX + vec3.xCoord * d1;
-					projectile.posY = this.entity.posY + (double)(this.entity.height / 2.0F) + 0.5D;
-					projectile.posZ = this.entity.posZ + vec3.zCoord * d1;
-					world.spawnEntityInWorld(projectile);
-					this.counter = -40;
-				}
-			}
-			else if(this.counter > 0) counter--;
-			this.entity.setFire(this.counter > 10);
-		}
-	}
-
-	@Override
-	public double setAttackDamage(MobStats s) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 }
