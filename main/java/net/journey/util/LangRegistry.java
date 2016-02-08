8c530714a7c4f30668fce1f8161077caf6b1fe66
Eternaldoom <eternaldoom123@gmail.com>
2016-02-07 21:22:27 -0800

    Fix annoying

--- main/java/net/journey/util/LangRegistry.java
+++ main/java/net/journey/util/LangRegistry.java
@@ -29,7 +29,6 @@ public class LangRegistry {
 			if(en_US.exists()) en_US.delete();
 			en_US.createNewFile();
 			writer = new BufferedWriter(new FileWriter(en_US));
-			Desktop.getDesktop().open(new File(location));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
